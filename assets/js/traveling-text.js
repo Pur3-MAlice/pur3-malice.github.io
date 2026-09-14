ec0d352243945e6f34980dc0ba355ac7f74ed288
Alice <114814174+Pur3-MAlice@users.noreply.github.com>
2026-09-14 13:03:02 +0000

    stylign chnages

--- assets/js/traveling-text.js
+++ assets/js/traveling-text.js
@@ -14,16 +14,16 @@ tl.fromTo(".repeatText",
     { attr: { startOffset: "100%" }, duration: dur, ease: "linear" }  // move to off-screen right
 );
 
-// const btn = document.getElementById("pauseBtn");
-// let paused = false;
+const btn = document.getElementById("pauseBtn");
+let paused = false;
 
-// btn.addEventListener("click", () => {
-//     if (!paused) {
-//         tl.pause();
-//         btn.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
-//     } else {
-//         tl.resume();
-//         btn.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
-//     }
-//     paused = !paused;
-// });
+btn.addEventListener("click", () => {
+    if (!paused) {
+        tl.pause();
+        btn.innerHTML = '<i class="fa fa-play" aria-hidden="true"></i>';
+    } else {
+        tl.resume();
+        btn.innerHTML = '<i class="fa fa-pause" aria-hidden="true"></i>';
+    }
+    paused = !paused;
+});
