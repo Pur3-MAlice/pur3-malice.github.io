734375dc1e011988d4640d7d4a263e3ebea37abc
Alice <114814174+Pur3-MAlice@users.noreply.github.com>
2026-09-14 10:48:12 +0000

    removing google fonts, introducing noindex and saving some styles

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -29,13 +29,13 @@ markerContent.innerHTML = `
     width="24" height="24" 
     viewBox="0 0 24 24" 
     fill="none" 
-    stroke="#818865" 
+    stroke="#213d63" 
     stroke-width="1" 
     stroke-linecap="round" 
     stroke-linejoin="round" 
     class="feather feather-map-pin">
-    <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z" fill="#818865" />
-    <circle cx="12" cy="10" r="3" fill="#38492d" />
+    <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z" fill="#7091BA" />
+    <circle cx="12" cy="10" r="3" fill="#213d63" />
     </svg>
 `;
 
